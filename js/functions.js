9398b726c125343db5d48d0b6d45241f76beec71
Tomasz Osuch <tomasz.osuch@gmail.com>
2025-12-17 11:22:32 +0100

    refactor: add new function amd fix game logic

--- js/functions.js
+++ js/functions.js
@@ -7,3 +7,61 @@ function printMessage(message) {
 function clearMessages() {
     document.getElementById('messages').innerHTML = '';
 }
+
+function getMoveName(argMoveId) {
+    console.log('Wywołano funkcję getMoveName z argumentem:', argMoveId);
+    if (argMoveId === 1) {
+        console.log('Wybrany ruch: kamień');
+        return 'kamień';
+    } else if (argMoveId === 2) {
+        console.log('Wybrany ruch: papier');
+        return 'papier';
+    } else if (argMoveId === 3) {
+        console.log('Wybrany ruch: nożyce');
+        return 'nożyce';
+    } else {
+        console.log('Nieprawidłowy ruch o id:', argMoveId);
+        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+        return 'nie znam tego ruchu';
+    }
+}
+
+function displayResult1(argComputerMove, argPlayerMove) {
+    if (argComputerMove === argPlayerMove) {
+        printMessage('Remis!');
+    } else if (
+        (argComputerMove === 'kamień' && argPlayerMove === 'nożyce') ||
+        (argComputerMove === 'papier' && argPlayerMove === 'kamień') ||
+        (argComputerMove === 'nożyce' && argPlayerMove === 'papier')
+    ) {
+        printMessage('Komputer wygrał!');
+    } else if (argPlayerMove === 'nie znam tego ruchu') {
+        printMessage('Nieprawidłowy ruch gracza.');
+    } else {
+        printMessage('Gratulacje! Wygrałeś!');
+    }
+}
+
+function displayResult(argComputerMove, argPlayerMove) {
+    console.log('moves:', argComputerMove, argPlayerMove);
+    if (argComputerMove === argPlayerMove) {
+        console.log('Wynik: Remis!');
+        printMessage('Remis!');
+    } else if (
+        (argComputerMove === 'kamień' && argPlayerMove === 'nożyce') ||
+        (argComputerMove === 'papier' && argPlayerMove === 'kamień') ||
+        (argComputerMove === 'nożyce' && argPlayerMove === 'papier')
+    ) {
+        console.log('Wynik: Komputer wygrał!');
+        printMessage('Komputer wygrał!');
+    } else if (argPlayerMove === 'nie znam tego ruchu') {
+        console.log('Wynik: Nieprawidłowy ruch gracza');
+        printMessage('Nieprawidłowy ruch gracza.');
+    } else {
+        console.log('Wynik: Gracz wygrał!');
+        printMessage('Gratulacje! Wygrałeś!');
+    }
+}
+
+// Test funkcji getMoveName
+console.log('Test getMoveName(2):', getMoveName(2));
--- js/script.js
+++ js/script.js
@@ -4,15 +4,8 @@ let randomNumber = Math.floor(Math.random() * 3) + 1;
 
 console.log('Wylosowana liczba całkowita z zakresu 1-3 to: ' + randomNumber);
 
-let computerMove = '';
-
-if (randomNumber === 1) {
-    computerMove = 'kamień';
-} else if (randomNumber === 2) {
-    computerMove = 'papier';
-} else if (randomNumber === 3) {
-    computerMove = 'nożyce';
-}
+let computerMove = getMoveName(randomNumber);
+console.log('Ruch komputera po getMoveName:', computerMove);
 
 printMessage('Ruch computera to: ' + computerMove);
 
@@ -22,28 +15,11 @@ let playerImput = prompt('Wybierz swój ruch! 1: kamień, 2: papier lub 3: noży
 
 console.log('Gracz wybrał: ' + playerImput);
 
-let playerMove = '';
-
-if (playerImput == '1') {
-    playerMove = 'kamień';
-} else if (playerImput == '2') {
-    playerMove = 'papier';
-} else if (playerImput == '3') {
-    playerMove = 'nożyce';
-}
+let playerMove = getMoveName(playerImput);
+console.log('Ruch gracza po getMoveName:', playerMove);
 
 printMessage('Twój ruch to: ' + playerMove);
 
 // Określenie wyniku gry
 
-if (computerMove === playerMove) {
-    printMessage('Remis!');
-} else if (
-    (computerMove === 'kamień' && playerMove === 'nożyce') ||
-    (computerMove === 'papier' && playerMove === 'kamień') ||
-    (computerMove === 'nożyce' && playerMove === 'papier')
-) {
-    printMessage('Komputer wygrał!');
-} else {
-    printMessage('Gratulacje! Wygrałeś!');
-}
+displayResult(computerMove, playerMove);
